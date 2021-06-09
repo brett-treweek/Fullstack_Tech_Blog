669fea78580afc019ed813d36113bf65c423c308
Brett Treweek <bretttrew@gmail.com>
2021-06-09 23:09:47 +0800

    dashboard and comments kinda working

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,46 +1,42 @@
 const router = require("express").Router();
-const { Posts, User } = require("../models");
+const { Posts, User, Comments } = require("../models");
 
 router.get("/", async (req, res) => {
   try {
     const postData = await Posts.findAll();
-    const posts = postData.map((post) => post.get({plain: true}))
-    res.render("homepage", {posts});
+    const posts = postData.map((post) => post.get({ plain: true }));
+    res.render("homepage", { posts });
   } catch (error) {
     res.status(500).json(error);
   }
 });
 
-router.get('/dashboard/:id', async (req, res) => {
-    try {
-        const userDashboard = await User.findByPk(req.params.id, {
-            include: [
-                {
-                    model: Posts,
-                    attributes: [
-                        'id',
-                        'title',
-                        'content',
-                        'date'
-                    ],
-                    // include: [
-                    //     {
-                    //         model: Comments,
-                    //         attributes: [
-                    //             'author',
-                    //             'content',
-                    //             'date'
-                    //         ]
-                    //     }
-                    // ]
-                }
-            ]
-        })
-        const user = userDashboard.get({ plain: true })
-        res.render('homepage')
-    } catch (error) {
-        res.status(500).json(error)
-    }
-})
+router.get("/dashboard/:id", async (req, res) => {
+  try {
+    const userDashboard = await User.findByPk(req.params.id, {
+      include: [
+        {
+          model: Posts,
+          include: [
+            {
+              model: Comments,
+            },
+          ],
+        },
+      ],
+    });
+
+    const posts = userDashboard.get({ plain: true });
+
+    console.log("HHHHHHHHHH-------", userDashboard);
+    console.log("ooooooooOOOOOO-------", posts);
+    console.log("IIIIIIIIII-------", posts.posts[0].comments[0]);
+
+    const comments = posts.posts[0].comments;
+    res.render("dashboard", { posts, comments });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 module.exports = router;
